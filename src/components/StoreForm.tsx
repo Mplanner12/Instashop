4bd69d4758dc52983d23a6761ad12f767c5d057d
Mustapha Shuaibu <almussanplanner12@gmail.com>
2025-01-12 16:56:49 +0100

    Remove unused Link import from StoreForm component

--- src/components/StoreForm.tsx
+++ src/components/StoreForm.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import Loader from "@/utils/Loader";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const StoreForm = () => {
